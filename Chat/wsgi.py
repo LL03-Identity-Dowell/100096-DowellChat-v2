761be9ee5a09ee1e7991f32a2da186e1d8850da2
Chidiebere Ibiam <chidiebereorjiibiam7@gmail.com>
2023-11-03 14:58:48 +0100

    Modified wsgi to fix sock binding issues

--- Chat/wsgi.py
+++ Chat/wsgi.py
@@ -1,17 +1,32 @@
+# import os
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chat.settings')
+# import socketio
+# from django.core.wsgi import get_wsgi_application
+#
+# django_app= get_wsgi_application()
+# from api.views import sio
+#
+# # application = socketio.WSGIApp(sio, django_app)
+# # Using eventlet to run the server
+# import eventlet
+# application = socketio.WSGIApp(sio, django_app)
+#
+# # Binding to the port
+# eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
+
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chat.settings')
 import socketio
-
-
 from django.core.wsgi import get_wsgi_application
-
-django_app= get_wsgi_application()
+django_app = get_wsgi_application()
 from api.views import sio
 
-# application = socketio.WSGIApp(sio, django_app)
 # Using eventlet to run the server
 import eventlet
-application = socketio.WSGIApp(sio, django_app)
 
-# Binding to the port
-eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
+def start_server():
+    application = socketio.WSGIApp(sio, django_app)
+    return application
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), start_server)
