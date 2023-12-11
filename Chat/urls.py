e290fdded1427a9a74ffcd5014629cd244aadd6d
Chidiebere Ibiam <chidiebereorjiibiam7@gmail.com>
2023-12-11 09:21:48 +0000

    Changes on Urls.py

--- Chat/urls.py
+++ Chat/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import SSLVerificationView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
