2d37c346829ec0e5337afe88300a061ecea82181
aklile-yilma <aklileyilma@gmail.com>
2024-01-04 12:41:32 +0300

    feat: chat-section UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,59 @@
-import LoginPage from "./pages/LoginPage";
-import { useState } from "react";
-import Dashboard from "./pages/Dashboard";
-import Avatar from "react-avatar";
+import { useState, useRef, useEffect } from "react";
 import { MdVideoCall } from "react-icons/md";
 import { IoCall } from "react-icons/io5";
 import SideBarUpdated from "./component/SideBarUpdated";
 import SearchSection from "./component/SearchSection";
+import ChatMessage from "./component/ChatMessage";
+import dummyImage from './assets/dummy-image-green.jpg'
 // import logo from "logo.jpg"
 
+const messages = [
+  { isSender: true, content: "Hello there!" },
+  { isSender: false, content: "Hi! How are you?" },
+  { isSender: true, content: "I'm doing well, thanks!" },
+  { isSender: false, content: "That's great to hear!" },
+  { isSender: true, content: "What have you been up to?" },
+  { isSender: false, content: "Just working on some projects. How about you?" },
+  { isSender: true, content: "I've been busy too, lots of meetings." },
+  { isSender: false, content: "Ah, the usual work stuff. Anything exciting happening?" },
+  { isSender: true, content: "Not much, just trying to stay productive." },
+  { isSender: true, content: "Hello there!" },
+  { isSender: false, content: "Hi! How are you?" },
+  { isSender: true, content: "I'm doing well, thanks!" },
+  { isSender: false, content: "That's great to hear!" },
+  { isSender: true, content: "What have you been up to?" },
+  { isSender: false, content: "Just working on some projects. How about you?" },
+  { isSender: true, content: "I've been busy too, lots of meetings." },
+  { isSender: false, content: "Ah, the usual work stuff. Anything exciting happening?" },
+  { isSender: true, content: "Not much, just trying to stay productive." },
+  { isSender: true, content: "Hello there!" },
+  { isSender: false, content: "Hi! How are you?" },
+  { isSender: true, content: "I'm doing well, thanks!" },
+  { isSender: false, content: "That's great to hear!" },
+  { isSender: true, content: "What have you been up to?" },
+  { isSender: false, content: "Just working on some projects. How about you?" },
+  { isSender: true, content: "I've been busy too, lots of meetings." },
+  { isSender: false, content: "Ah, the usual work stuff. Anything exciting happening?" },
+  { isSender: false, type: 'image', imagePath: dummyImage },
+
+  { isSender: true, content: "Not much, just trying to stay productive." },
+];
+
+
 export default function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
+  const scrollContainerRef = useRef(null);
+  const [chatInput, setChatInput] = useState('');
 
   const toggleSection = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollTop = scrollContainerRef.current.scrollHeight;
+    }
+  }, []);
 
   return (
     <div className="h-screen flex flex-row bg-gray-300">
@@ -23,7 +63,7 @@ export default function App() {
           <SearchSection isOpen={isOpen} />
         )}
 
-        <div className={`${isOpen ? 'col-span-8' : 'col-span-10'} ml-3 flex flex-col`}>
+        <div className={`${isOpen ? 'col-span-8' : 'col-span-10'} ml-3 flex flex-col max-h-screen`}>
           <div className="flex justify-between p-4 bg-[#F1F3F4] border-b-2 border-gray-300 rounded-t-3xl">
             <div>
               <svg
@@ -31,9 +71,9 @@ export default function App() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="w-12 h-12"
                 onClick={toggleSection}
               >
@@ -75,9 +115,20 @@ export default function App() {
             </div>
           </div>
 
-          <section className="h-full bg-white">
-
+          <section ref={scrollContainerRef}  className="flex-grow overflow-y-scroll bg-white px-2">
+            <div className="flex flex-col space-y-2 justify-end">
+              {
+                messages.map((message, index) => (
+                  <ChatMessage key={index} message={message}/>
+                ))
+              }
+            </div>
           </section>
+
+          <div className="flex items-center p-4 bg-white">
+            <input type="text" placeholder="Type here ..." value={chatInput} onChange={() => setChatInput(chatInput)} className="w-full rounded-lg border border-gray-300 px-4 py-2"/>
+            <button className="ml-2 rounded-lg bg-blue-500 px-4 py-2 text-white">Send</button>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/component/ChatMessage.jsx
@@ -0,0 +1,23 @@
+import { IoIosCheckmark } from "react-icons/io";
+
+const ChatMessage = ({message}) => {
+
+    return (
+        <div className={`${message?.isSender ? 'self-end' : 'self-start'}`}>
+                { 
+                message?.type == 'image' ? (
+                    <div className="w-60 mx-10 my-5">
+                        <img src={message?.imagePath} className="w-full object-cover rounded-lg" alt='image'/>
+                    </div> 
+                    ) : (
+                        <div className={`${message?.isSender ? 'bg-green-300 rounded-full rounded-tr' : 'bg-blue-300 rounded-full rounded-tl self-start'} flex items-center text-black/50 shadow-lg py-2 px-3`}>
+                            <IoIosCheckmark size={30} />
+                            <p>{message?.content}</p>
+                        </div>
+                    )
+                }
+        </div>
+    );
+}
+
+export default ChatMessage;
