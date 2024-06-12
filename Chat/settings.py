5ee241db3c4ff90440fc22f233a2f242e3f61e43
Chidiebere Ibiam <chidiebereorjiibiam7@gmail.com>
2024-06-12 09:09:28 +0100

    Fix: Swagger staticfile issues

--- Chat/settings.py
+++ Chat/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #MEDIA
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
-MEDIA_URL = '/media/'
+MEDIA_URL = '/api/media/'
 
 API_KEY = os.getenv("API_KEY")
 
