025c379d23ae625588cd986b46efe83e073d4fe3
Chidiebere Ibiam <chidiebereorjiibiam7@gmail.com>
2024-08-03 11:58:39 +0100

    "Refactored get_all_line_managers function to use LineManager model instead of data_cube and removed unnecessary checks"

--- websocket/views.py
+++ websocket/views.py
@@ -2050,37 +2050,19 @@ def get_all_line_managers(sid, message):
     try:
         workspace_id = message['workspace_id']
         api_key = message['api_key']
-        
-
         db_name = f"{workspace_id}_cs_ticketing_system_db0"
-        coll_name = f"{workspace_id}_line_manager"
 
         if not check_db(workspace_id, api_key, db_name):
             return sio.emit('setting_response', {'data':f"DB {db_name} Not found", 'status': 'failure', 'operation':'get_all_line_managers'}, room=sid)
 
-        if check_collection(api_key, workspace_id, coll_name, db_name):
-
-            response = data_cube.fetch_data(
-                api_key=api_key,
-                db_name=db_name,
-                coll_name=coll_name,
-                filters={},
-                limit=199,
-                offset=0
-            )
-        
-            if response['success']:
-                if not response['data']:
-                    return sio.emit('setting_response', {'data': 'No Line Manager found for this Workspace', 'status': 'failure', 'operation': 'get_all_line_managers'}, room=sid)
-
-                else:
-                    return sio.emit('setting_response', {'data': response['data'], 'status': 'success', 'operation': 'get_all_line_managers'}, room=sid)
-            else:
-                # Error in fetching data
-                return sio.emit('setting_response', {'data': response['message'], 'status': 'failure', 'operation': 'get_all_line_managers'}, room=sid)
+        line_managers = LineManager.objects.filter(workspace__org_id=workspace_id)
+        if line_managers:
+            serializer = LineManagerSerializer(line_managers, many=True)
+            return sio.emit('setting_response', {'data': serializer.data, 'status': 'success', 'operation': 'get_all_line_managers'}, room=sid)
+        else:
+            return sio.emit('setting_response', {'data': 'No Line Manager found for this Workspace', 'status': 'failure', 'operation': 'get_all_line_managers'}, room=sid)
 
     except Exception as e:
-        # Handle other exceptions
         error_message = str(e)
         return sio.emit('setting_response', {'data': error_message, 'status': 'failure', 'operation': 'get_all_line_managers'}, room=sid)
 
