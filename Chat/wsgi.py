ab71e6650ac812487b8aac86c11ba9242cc0c69c
unknown <ericmbuthia11@gmail.com>
2024-09-29 16:49:03 +0300

    Readjusted the wsgi logic for the importation

--- Chat/wsgi.py
+++ Chat/wsgi.py
@@ -1,25 +1,32 @@
+import django
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chat.settings')
-import socketio
 
-from django.core.wsgi import get_wsgi_application
-
+django.setup()
+from websocket.views import sio #noqa
+from django.core.wsgi import get_wsgi_application #noqa
+import socketio #noqa
+from api.utils.datacube_utils import check_connection #noqa
 # Get the Django WSGI application
+
 django_app = get_wsgi_application()
 
-from websocket.views import sio
-from api.utils.datacube_utils import check_connection
 
 socket_connection = check_connection()
 if socket_connection:
+    print("Connected Safely...")
     application = socketio.WSGIApp(sio, django_app)
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    pywsgi.WSGIServer(('0.0.0.0', 8001), application,
-                    handler_class=WebSocketHandler).serve_forever()
+    # from gevent import pywsgi
+    # from geventwebsocket.handler import WebSocketHandler
+    # pywsgi.WSGIServer(('0.0.0.0', 8001), application,
+    #                   handler_class=WebSocketHandler).serve_forever()
 else:
-    application = get_wsgi_application()
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    pywsgi.WSGIServer(('0.0.0.0', 8001), application,
-                    handler_class=WebSocketHandler).serve_forever()
+    print("Connection failed...")
+    # application = get_wsgi_application()
+    application = django_app
+
+from gevent import pywsgi #noqa
+from geventwebsocket.handler import WebSocketHandler #noqa
+
+pywsgi.WSGIServer(('0.0.0.0', 8001), application,
+                  handler_class=WebSocketHandler).serve_forever()
